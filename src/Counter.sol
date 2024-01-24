a3c443c0f15d41d1179aa343f733b6ae04be1f90
Udit Yadav <udit.yadav@codezeros.com>
2024-01-24 19:02:48 +0530

    asset tokeniser

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import 'openzeppelin-contracts/contracts/token/ERC721/ERC721.sol';
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract AssetTokeniser is ERC721 {
+    struct Property {
+        uint256 propertyId;
+        uint256 valuation;
+        uint256 investiblePercentage;
+    }
+    mapping(address => Property) public listedProperties;
+    mapping(uint256 => address) public investment;
+    constructor() ERC721("Asset Tokeniser", "ATR") {}
+
+    function listProperty(uint256 _propertyId, uint256 _valuation, uint256 _investiblePercentage) external {
+        listedProperties[msg.sender].propertyId = _propertyId;
+        listedProperties[msg.sender].valuation = _valuation;
+        listedProperties[msg.sender].investiblePercentage = _investiblePercentage;
+
+        //mint nft and tokens
     }
 
-    function increment() public {
-        number++;
+    function investInProperty(uint256 _propertyId) public payable{
+        investment[_propertyId] = msg.sender;
     }
 }
